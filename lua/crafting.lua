5357c1ede10ca78da3298034b661e41a67841034
rodolphecombe <rodolphe.combe@mailoo.org>
2024-03-11 15:13:43 +0100

    Updating to LotI 3.2.7d

--- lua/crafting.lua
+++ lua/crafting.lua
@@ -573,7 +573,8 @@ loti.gem.show_crafting_window = function(x, y)
 						populate_item(_"Gauntlets", "icons/gauntlets.png", "gauntlets")
 
 						word_from = 531
-						word_to = 560
+						-- End number is +1 because numbers_between() returns a half-open interval
+						word_to = 560+1
 					else
 						-- Weapon
 						populate_item(_"Sword", "attacks/sword-steel.png", "sword")
@@ -592,14 +593,15 @@ loti.gem.show_crafting_window = function(x, y)
 						populate_item(_"Otherworldly essence (for touch attacks, baneblade, ...)", "attacks/touch-faerie.png", "essence")
 
 						word_from = 561
-						word_to = 600
+						-- End number is +1 because numbers_between() returns a half-open interval
+						word_to = 600+1
 					end
 
 					-- Clear the recipe listbox
 					dialog.gui_recipe_chosen:remove_items_at(1, 0)
 
 					order = 1
-					for _, i in ipairs(loti.item.type.numbers_between(word_from, word_to + 1)) do
+					for _, i in ipairs(loti.item.type.numbers_between(word_from, word_to)) do
 						local item = loti.item.type[i]
 						dialog.gui_recipe_chosen[order].gui_recipe_name.label = item.name
 						if can_craft(i) then
